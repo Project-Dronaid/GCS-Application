f4078e205aaa9276f94d5341cffb50fa2a491f52
avikshitdhar <avikshitdhar75@gmail.com>
2025-07-27 20:48:11 +0530

    Added Buttons List

--- lib/utils/items.dart
+++ lib/utils/items.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-List<String> dropDownItems = [
-  'Loiter_Unm',
+//dropDown Menu Names
+List<String> doActionItems = [
+  'Loiter_Unlim',
   'Return_To_Launch',
   'Preflight_Calibration',
   'Mission_Start',
@@ -9,11 +10,56 @@ List<String> dropDownItems = [
   'Trigger_Camera',
   'System_Time',
   'Battery_Reset',
-  'ADSB_Out_ldent',
+  'ADSB_Out_Ident',
   'Scripting_cmd_stop_and_restart',
   'Scripting_cmd_stop',
   'HighLatency_Enable',
   'HighLatency_Disable',
-  'Toggle_Safety Switch',
-  'Do Parachute'
+  'Toggle_Safety_Switch',
+  'Do_Parachute'
 ];
+
+List<String> setModeItems = [
+  'MANUAL',
+  'CIRCLE',
+  'STABILIZE',
+  'TRAINING',
+  'ACRO',
+  'FBWA',
+  'FBWB',
+  'CRUISE',
+  'AUTOTUNE',
+  'Auto',
+  'RTL',
+  'Loiter',
+  'TAKEOFF',
+  'AVOID_ADSB',
+  'Guided',
+  'QSTABILIZE',
+  'QHOVER',
+  'QLOITER',
+  'QLAND',
+  'QRTL',
+  'QAUTOTUNE',
+  'QACRO',
+  'THERMAL',
+  'Loiter_to_QLand',
+  'AUTOLAND',
+  'INITIALISING'
+];
+
+// button names
+String doAction = 'Do Action';
+String auto = 'Auto';
+String setHomeAlt = 'Set Home Alt';
+String setWPAlt = 'Set WP';
+String loiter = 'Loiter';
+String restartMission = 'Restart Mission';
+String setMode = 'Set Mode';
+String rtl = 'RTL';
+String setMount = 'Set Mount';
+String joystick = 'Joystick';
+String armToggle = 'Arm/Disarm';
+String clearTrack = 'Clear Track';
+String resumeMission = 'Resume Mission';
+String abortLanding = 'Abort Landing';
